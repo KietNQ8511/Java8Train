5a3a87a1424b260f5a8a22235ad2c1ba60f51593
KIET QUOC NGUYEN/LGEDV CORE FRAMEWORK Kiet.nguyen FUNCTIONAL TECHNOLOGY TEAM(kiet.nguyen@lge.com) <kiet.nguyen@lge.com>
2023-10-06 17:07:09 +0700

    git add generic package

---
+++ src/generic/method/GenericMethod.java
@@ -0,0 +1,10 @@
+package generic.method;
+
+public class GenericMethod {
+
+	/**
+	 * We write generic methods with a single method declaration, and we can call them with arguments of different types.
+	 * The compiler will ensure the correctness of whichever type we use
+	 */
+
+}
